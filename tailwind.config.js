5249f5ef77b92fcde5e0b7c8775865df5ee2502e
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2023-08-19 11:39:17 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,19 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "yellow": "#ffb21e",
+        "blue": "#1125d6",
+        "green": "#00bb8f",
+        "red": "#ff5555",
+        "dark-navy": "#303b59",
+        "light-blue": "#cac9ff",
+        "very-light-blue": "#ecf2ff"
       },
+      gradientColorStops: {
+        1: "from-[#6943ff] to-[#2f2ce9]",
+        2: "from-[#4d21c9]"
+      }
     },
   },
   plugins: [],
