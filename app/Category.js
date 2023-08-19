dbdf9df869df6ecda2058d4c9507b1082ca2a909
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2023-08-19 18:12:06 +0200

    Update Category.js

--- app/Category.js
+++ app/Category.js
@@ -1,9 +1,19 @@
 import Image from "next/image"
 
-export default ({ icon, category, score }) => (
-    <div className="flex items-center">
-        <Image src={icon} width="20" height="20" />
-        <p className="grow">{category}</p>
-        <p>{score} / 100</p>
-    </div>
-)
+export default ({ icon, category, score, color }) => {
+    
+    const colors = {
+        red: "text-red bg-red/5",
+        yellow: "text-yellow bg-yellow/5",
+        green: "text-green bg-green/5",
+        blue: "text-blue bg-blue/5",
+    }
+
+    return (
+        <div className={`flex items-center h-14 rounded-xl p-4 gap-x-3 ${colors[color]}`}>
+            <Image src={icon} width="20" height="20" />
+            <p className="font-medium grow" >{category}</p>
+            <p className="font-bold text-dark-navy">{score}<span className="opacity-50"> / 100</span></p>
+        </div>
+    )
+}
