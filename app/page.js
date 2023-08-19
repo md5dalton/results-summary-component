226f729e22ab5c4e3bdb1c22c0ba70c8c76a63f5
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2023-08-19 10:57:50 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,3 +1,28 @@
+import Image from "next/image";
+
 export default () => (
-  <p>home</p>
+    <main>
+        <section>
+            <p>Your Result</p>
+            <div>
+                <p>76</p>
+                <p>of 100</p>
+            </div>
+            <p>Great</p>
+            <p>Your performance exceed 65% of the peaople conducting the test here!</p>
+        </section>
+        <section>
+            <p>Summary</p>
+            <ul>
+                <li>
+                    <Image src="/icon-reaction.svg" width="20" height="20" />
+                    <p>Reaction</p>
+                    <p>80 / 100</p>
+                </li>
+            </ul>
+        </section>
+        <button>Continue</button>
+    </main>
+  
+  
 )
