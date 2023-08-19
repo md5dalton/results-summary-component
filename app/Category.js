8f59fe24ef401f0c1a19c4ae82e529d384028e1a
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2023-08-19 11:39:22 +0200

    Update Category.js

--- app/Category.js
+++ app/Category.js
@@ -1,9 +1,9 @@
 import Image from "next/image"
 
-export default ({ icon, name, score }) => (
-    <div>
+export default ({ icon, category, score }) => (
+    <div className="flex items-center">
         <Image src={icon} width="20" height="20" />
-        <p>{name}</p>
+        <p className="grow">{category}</p>
         <p>{score} / 100</p>
     </div>
 )
