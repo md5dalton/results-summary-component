3ed86d2c9f44811bf14b03e4a92f28d27e9f37a7
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2023-08-20 13:31:48 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -14,11 +14,16 @@ export default () => (
                 bg-gradient-to-b from-[#6943ff] to-[#2f2ce9] 
                 rounded-b-[2rem]
                 shadow-[0_30px_60px_rgba(61,108,236,0.15)]
+
+                transition
+
+                cursor-pointer
                 
                 md:gap-y-9 md:py-10
                 md:px-[40px]
                 md:rounded-[2rem]
-                md:shadow-none"
+                md:shadow-none
+                hover:from-transparent hover:to-transparent"
         >
             <p className="font-bold text-lg md:text-2xl">Your Result</p>
             <div className="rounded-full bg-gradient-to-b from-[#4d21c9] h-[140px] w-[140px] md:h-[200px] md:w-[200px] grid place-content-center">
@@ -27,7 +32,7 @@ export default () => (
             </div>
             <div className="max-w-[260px] grid gap-2 md:gap-3">
                 <p className="text-2xl md:text-[2rem] font-bold">Great</p>
-                <p className="text-light-blue md:text-lg">Your performance exceed 65% of the peaople conducting the test here!</p>
+                <p className="text-light-blue md:text-lg">Your performance exceed 65% of the people conducting the test here!</p>
             </div>
         </section>
         <section className="
@@ -44,7 +49,7 @@ export default () => (
                 items={data}
                 itemHandler={item => <Category {...item} />}
             />
-            <button className="text-white bg-dark-navy rounded-[8rem] h-14 w-full font-bold md:text-lg">Continue</button>
+            <button className="text-white bg-dark-navy rounded-[8rem] h-14 w-full font-bold md:text-lg hover:bg-gradient-to-b transition from-[#6943ff] to-[#2f2ce9]">Continue</button>
         </section>
     </main>
 )
